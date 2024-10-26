291346bbcde14eb9fb4499a463282f97eb7578fd
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2024-10-26 18:06:33 -0300

    :rocket: deploy: nova tentativa
    O render deletou tudo, e agora o banco de dados não funciona. Alterando o arquivo render para ver se isso muda.

---
+++ db/calvo_das_paixoes.sql
@@ -0,0 +1,3 @@
+GRANT CREATE ON SCHEMA public TO calvo_das_paixoes;
+GRANT USAGE ON SCHEMA public TO calvo_das_paixoes;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE telecom, ti, serv_audiovisuais, ed_e_ed_integradas_a_impressao, agencia_noticias TO calvo_das_paixoes;
--- db/pizza_man.sql
+++
@@ -1,3 +0,0 @@
-GRANT CREATE ON SCHEMA public TO pizza_man;
-GRANT USAGE ON SCHEMA public TO pizza_man;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE telecom, ti, serv_audiovisuais, ed_e_ed_integradas_a_impressao, agencia_noticias TO pizza_man;
